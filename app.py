448eb578cde9c6fccaf8604bf7be4906e2189f7e
soumyajindal07 <54813070+soumyajindal07@users.noreply.github.com>
2026-01-16 17:00:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,3 @@ async def start_conversation(prompt: str, isConnectedAppProvided: bool):
     except Exception as e:
         return Response(statuscode = 500, content=str(e))
 
-if __name__ == "__main__":    
-    uvicorn.run(app, host = "0.0.0.0", port = 8000)
